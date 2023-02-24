32946b59fe11e70dc3a904fd0634149c9dbae9e9
amanirshad <aman.itz.me07@gmail.com>
2023-02-25 01:14:13 +0530

    Use Mapping Libraries in EmployeeService and DepartmentService fix

--- department-service/src/main/java/aman/irshad/departmentservice/mapper/AutoDepartmentMapper.java
+++ department-service/src/main/java/aman/irshad/departmentservice/mapper/AutoDepartmentMapper.java
@@ -2,8 +2,10 @@ package aman.irshad.departmentservice.mapper;
 
 import aman.irshad.departmentservice.dto.DepartmentDto;
 import aman.irshad.departmentservice.entity.Department;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface AutoDepartmentMapper {
 
     AutoDepartmentMapper MAPPER = Mappers.getMapper(AutoDepartmentMapper.class);
--- employee-service/src/main/java/aman/irshad/employeeservice/mapper/AutoEmployeeMapper.java
+++ employee-service/src/main/java/aman/irshad/employeeservice/mapper/AutoEmployeeMapper.java
@@ -2,8 +2,10 @@ package aman.irshad.employeeservice.mapper;
 
 import aman.irshad.employeeservice.dto.EmployeeDto;
 import aman.irshad.employeeservice.entity.Employee;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface AutoEmployeeMapper {
 
     AutoEmployeeMapper MAPPER = Mappers.getMapper(AutoEmployeeMapper.class);
