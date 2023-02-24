8f77bb97ff899cb994aa42d8f6f2199277d852cc
amanirshad <aman.itz.me07@gmail.com>
2023-02-25 00:47:23 +0530

    Using ModelMapper library to Map Entity to DTO and Vice Versa

--- department-service/src/main/java/aman/irshad/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/aman/irshad/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -5,6 +5,7 @@ import aman.irshad.departmentservice.entity.Department;
 import aman.irshad.departmentservice.repository.DepartmentRepository;
 import aman.irshad.departmentservice.service.DepartmentService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +15,26 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private DepartmentRepository departmentRepository;
 
+    private ModelMapper modelMapper;
 
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
 
         //convert departmentDto to department JPA Entity
-        Department department = new Department( departmentDto.getDepartmentName(),
-                departmentDto.getDepartmentDescription(), departmentDto.getDepartmentCode());
+//        Department department = new Department( departmentDto.getDepartmentName(),
+//                departmentDto.getDepartmentDescription(), departmentDto.getDepartmentCode());
+
+        Department department = modelMapper.map(departmentDto,Department.class);
 
         Department savedDepartment = departmentRepository.save(department);
 
         //convert department JPA Entity to departmentDto
-        DepartmentDto savedDepartmentDto = new DepartmentDto(
-                savedDepartment.getId(), savedDepartment.getDepartmentName(), savedDepartment.getDepartmentDescription(),
-                savedDepartment.getDepartmentCode()
-        );
+//        DepartmentDto savedDepartmentDto = new DepartmentDto(
+//                savedDepartment.getId(), savedDepartment.getDepartmentName(), savedDepartment.getDepartmentDescription(),
+//                savedDepartment.getDepartmentCode()
+//        );
+
+        DepartmentDto savedDepartmentDto = modelMapper.map(savedDepartment,DepartmentDto.class);
         return savedDepartmentDto;
 
     }
@@ -37,11 +43,12 @@ public class DepartmentServiceImpl implements DepartmentService {
     public DepartmentDto getDepartmentByCode(String departmentCode) {
         Department department = departmentRepository.findByDepartmentCode(departmentCode);
 
-        DepartmentDto departmentDto = new DepartmentDto(
-                department.getId(), department.getDepartmentName(),
-                department.getDepartmentDescription(),
-                department.getDepartmentCode()
-        );
+//        DepartmentDto departmentDto = new DepartmentDto(
+//                department.getId(), department.getDepartmentName(),
+//                department.getDepartmentDescription(),
+//                department.getDepartmentCode()
+//        );
+        DepartmentDto departmentDto = modelMapper.map(department,DepartmentDto.class);
         return departmentDto;
 
     }
--- employee-service/src/main/java/aman/irshad/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/aman/irshad/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import aman.irshad.employeeservice.entity.Employee;
 import aman.irshad.employeeservice.repository.EmployeeRepository;
 import aman.irshad.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,19 +14,25 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
+    private ModelMapper modelMapper;
+
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
 
-        Employee employee = new Employee(employeeDto.getFirstName(),
-                employeeDto.getLastName(), employeeDto.getEmail());
+//        Employee employee = new Employee(employeeDto.getFirstName(),
+//                employeeDto.getLastName(), employeeDto.getEmail());
+
+        Employee employee = modelMapper.map(employeeDto,Employee.class);
 
         Employee savedEmployee = employeeRepository.save(employee);
 
-        EmployeeDto savedEmployeeDto = new EmployeeDto(savedEmployee.getId(),
-                savedEmployee.getFirstName(),
-                savedEmployee.getLastName(),
-                savedEmployee.getEmail());
+
+        EmployeeDto savedEmployeeDto = modelMapper.map(savedEmployee,EmployeeDto.class);
+//        EmployeeDto savedEmployeeDto = new EmployeeDto(savedEmployee.getId(),
+//                savedEmployee.getFirstName(),
+//                savedEmployee.getLastName(),
+//                savedEmployee.getEmail());
 
         return savedEmployeeDto;
     }
