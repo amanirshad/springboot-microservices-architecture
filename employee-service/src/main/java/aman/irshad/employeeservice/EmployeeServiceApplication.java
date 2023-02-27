bc12bad92e68a60b3cb5974ec539343c73e0d6fd
amanirshad <aman.itz.me07@gmail.com>
2023-02-27 23:52:26 +0530

    Microservices Communication using WebClient

--- employee-service/src/main/java/aman/irshad/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/aman/irshad/employeeservice/EmployeeServiceApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
+
+	@Bean
+	public WebClient webClient(){
+		return WebClient.builder().build();
+	}
 
 	@Bean
 	public RestTemplate restTemplate(){
--- employee-service/src/main/java/aman/irshad/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/aman/irshad/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -21,6 +22,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
 
     private RestTemplate restTemplate;
+
+    private WebClient webClient;
 
     private ModelMapper modelMapper;
 
@@ -53,10 +56,16 @@ public class EmployeeServiceImpl implements EmployeeService {
                 () -> new ResourceNotFoundException("Employee","id",employeeId)
         );
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+       DepartmentDto departmentDto =  webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
 //        EmployeeDto employeeDto = new EmployeeDto(
 //                employee.getId(),
