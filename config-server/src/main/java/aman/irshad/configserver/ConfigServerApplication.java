fbea0cd7d57dcc5276e048ecf1eb44ddb0daf325
amanirshad <aman.itz.me07@gmail.com>
2023-03-02 00:43:16 +0530

    Create and Setup Spring Cloud Config Server

---
+++ config-server/src/main/java/aman/irshad/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package aman.irshad.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ config-server/src/test/java/aman/irshad/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package aman.irshad.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
