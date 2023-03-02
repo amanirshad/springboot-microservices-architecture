e560a63275d0aa38945438707c31ac475851aa1c
amanirshad <aman.itz.me07@gmail.com>
2023-03-03 01:23:42 +0530

    Retry Pattern implementation using Resilience4j

--- employee-service/src/main/java/aman/irshad/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/aman/irshad/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,8 +10,11 @@ import aman.irshad.employeeservice.repository.EmployeeRepository;
 import aman.irshad.employeeservice.service.APIClient;
 import aman.irshad.employeeservice.service.EmployeeService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -28,6 +31,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     private WebClient webClient;
 
     private APIClient apiClient;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     private ModelMapper modelMapper;
 
@@ -55,7 +60,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    @CircuitBreaker(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
+    //@CircuitBreaker(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResourceNotFoundException("Employee","id",employeeId)
@@ -65,7 +71,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 //                DepartmentDto.class);
 //
 //        DepartmentDto departmentDto = responseEntity.getBody();
-
+        LOGGER.info("Inside getEmployeeById() Method");
        DepartmentDto departmentDto =  webClient.get()
                 .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
                 .retrieve()
@@ -93,6 +99,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     // fallback method implementation
     public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception){
+        LOGGER.info("Inside getDefaultDepartment() Method");
 
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResourceNotFoundException("Employee","id",employeeId)
